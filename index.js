ac405fcc3f15ba6e9178203f74189bec8430cc82
david <david_boman@hotmail.se>
2015-11-10 13:50:54 +0100

    Demo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var pg = require('pg');
+
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,6 +12,18 @@ app.use(express.static(__dirname + '/public'));
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+})
 
 app.get('/', function(request, response) {
   var result = ''
